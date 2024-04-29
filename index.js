d58248aac628892eab8b9c27d4602160d7649870
Francisco Soares <franciscosoares13@hotmail.com>
2024-04-29 14:33:31 +0100

    WD APR24 FRANCISCO SOARES

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+   
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+  
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+    for(let i =0;i<this.length;i++) {
+      if(i === pos) {
+        return this.items[i]
+      }
+      else if(pos > this.items.length -1){
+              throw new Error("OutOfBounds");
+        }
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      const crescentArr = this.items.sort((a,b) => a-b);
+      return crescentArr.splice(this.length-1,1);
 
-  min() {}
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  min() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      const crescentArr = this.items.sort((a,b) => b-a);
+      return crescentArr.splice(this.length-1,1);
+
+    }
+
+  }
+
+  sum() {
+    if(this.length=== 0) {
+    return 0}
+    else  {
+      return this.items.reduce((acc,number)=> {return acc + number},0)
+    }
+
+  }
+
+  avg() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    else{
+      const sum = this.items.reduce((acc,number)=> {return acc + number},0);
+      return sum/this.length;
+    }  
+  }
 }
+
 
 module.exports = SortedList;
